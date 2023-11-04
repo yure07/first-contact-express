f81d36dda56549a4c06b1de89e0c35d330b48823
Yure Rafael <rafaelyure53@gmail.com>
2023-11-04 15:02:18 -0300

    feat: modularizing database

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
+const db = require('./dbConcessionaria')
+const dbVechiles = db.veiculosDatabase()
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 
-const vechiles = []
-
-app.get('/vechiles', (req, res) => {
-    res.status(200).send({vechiles: vechiles})
-    console.log({vechiles: vechiles})
+app.get('/vechiles', async (req, res) => {
+    const vechiles = await dbVechiles.list()
+    res.status(200).send(vechiles)
 })
 
-app.get('/vechiles/:id', (req, res) => {
-    const vechileId = req.params.id
-    const vechileFounded = vechiles.find(vechile => vechile.id == vechileId)
-    res.status(200).send(vechileFounded)
-    console.log(vechileFounded)
+app.get('/vechiles/:id', async (req, res) => {
+    const vechile = await dbVechiles.get(req.params.id)
+    res.status(200).send(vechile)
 })
 
-app.post('/vechiles', (req, res) =>{
-    res.status(200).send(req.body)
-    vechiles.push(req.body)
-    console.log(req.body)
+app.post('/vechiles', async (req, res) =>{
+    const vechileToPost = await dbVechiles.insert(req.body)
+    res.status(200).send(vechileToPost)
 })
 
-app.put('/vechiles/:id', (req, res) => {
-    const vechileId = req.params.id
-    const vechileToEdit = vechiles.find(vechile => vechile.id == vechileId)
-    const indexVechile = vechiles.indexOf(vechileToEdit)
-    vechiles[indexVechile] = req.body
-    res.status(200).send('veiculo foi atualizado')
-    console.log(req.body)
+app.put('/vechiles/:id', async (req, res) => {
+    const vechileUpdated = await dbVechiles.update(req.body, req.params.id)
+    res.status(200).send(vechileUpdated)
 })
 
-app.delete('/vechiles/:id', (req, res) => {
-    const vechileId = req.params.id
-    const vechileToDelete = vechiles.find(vechile => vechile.id == vechileId)
-    const indexVechile = vechiles.indexOf(vechileToDelete)
-    vechiles.splice(indexVechile, 1)
-    res.status(200).send(`veiculo de id ${vechileId} foi deletado com sucesso`)
+app.delete('/vechiles/:id', async (req, res) => {
+    const vechileToDelete = await dbVechiles.del(req.params.id)
+    res.status(200).send(vechileToDelete)
 })
 
-app.listen('3000', () => {
+app.listen(3000, () => {
     console.log('Servidor está aberto na porta 3000')
 })
